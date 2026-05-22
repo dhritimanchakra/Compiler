c05406d5117a4a21a3a0d5a8641eefcbf689a33f
Dhrimanti <dhritimancg@gmail.com>
2026-05-22 18:17:17 +0530

    Still going on

--- type.c
+++ type.c
@@ -3,3 +3,32 @@
 #include <asseert.h>
 #include <stdlib.h>
 #include <stdlib.h>
+
+static TYPE_LIST* currentTypeList=0;
+
+TYPE_INFO* getBaseType(TYPE_INFO* typeInfo){
+    return (typeInfo->type==array_t)?typeInfo->info.array.base:typeInfo;
+}
+
+int areTypesEqual(TYPE_INFO* t1,TYPE_INFO* t2){
+    if(t1==t2){
+        return 1;
+
+    }
+    else if(t1->type!=t2->type){
+        return 0;
+    }
+
+    switch(t1->type){
+        case int_t:
+        case address_t:
+        case char_t:
+            return 1;
+        case array_t:
+            return areTypesEqual(t1->info.array.base,t2->info.array.base);
+
+        case 
+
+    }
+    
+}
