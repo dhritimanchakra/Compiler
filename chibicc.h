9561c2e9ee608733f2bbb540e3a30b42a7d27984
Dhrimanti <dhritimancg@gmail.com>
2026-05-22 20:17:32 +0530

    Still working on this:

--- chibicc.h
+++ chibicc.h
@@ -89,3 +89,101 @@ File *new_file(char *name,int file_no,char *contents);
 Token *tokenize_string_literal(Token *tok,Type *basety);
 Token *tokenize(File *file);
 Token *tokenize_file(char *filename);
+
+#define unreachable(){
+    error("internal error at %s:%d",__FILE__,__LINE__);
+}
+
+
+char *search_include_paths(char *filename);
+void init_macros(void);
+void define_macro(char *name,char *buf);
+void underf_macro(char *name);
+Token *preprocess(Token *tok);
+
+
+typedef struct Obj Obj;
+
+struct Obj{
+    Obj *next;
+    char *name;
+    Type *ty;
+    Token *tok;
+    bool is_local;
+    int align;
+    int offset;
+    bool is_function;
+    bool is_tls;
+    char *init_data;
+    Relocation *rel;
+    bool is_inline;
+    Obj *params;
+    Node *body;
+    Obj *locals;
+    Obj *va_area;
+    Obj *alloca_bottom;
+    int stack_size;
+    bool is_live;
+    bool is_root;
+    StringArray refs;
+};
+
+typedef struct Relocation Relocation;
+
+struct Relocation{
+    Relocation *next;
+    int offset;
+    char **label;
+    int addend;
+};
+
+typedef enum {
+  ND_NULL_EXPR, // Do nothing
+  ND_ADD,       // +
+  ND_SUB,       // -
+  ND_MUL,       // *
+  ND_DIV,       // /
+  ND_NEG,       // unary -
+  ND_MOD,       // %
+  ND_BITAND,    // &
+  ND_BITOR,     // |
+  ND_BITXOR,    // ^
+  ND_SHL,       // <<
+  ND_SHR,       // >>
+  ND_EQ,        // ==
+  ND_NE,        // !=
+  ND_LT,        // <
+  ND_LE,        // <=
+  ND_ASSIGN,    // =
+  ND_COND,      // ?:
+  ND_COMMA,     // ,
+  ND_MEMBER,    // . (struct member access)
+  ND_ADDR,      // unary &
+  ND_DEREF,     // unary *
+  ND_NOT,       // !
+  ND_BITNOT,    // ~
+  ND_LOGAND,    // &&
+  ND_LOGOR,     // ||
+  ND_RETURN,    // "return"
+  ND_IF,        // "if"
+  ND_FOR,       // "for" or "while"
+  ND_DO,        // "do"
+  ND_SWITCH,    // "switch"
+  ND_CASE,      // "case"
+  ND_BLOCK,     // { ... }
+  ND_GOTO,      // "goto"
+  ND_GOTO_EXPR, // "goto" labels-as-values
+  ND_LABEL,     // Labeled statement
+  ND_LABEL_VAL, // [GNU] Labels-as-values
+  ND_FUNCALL,   // Function call
+  ND_EXPR_STMT, // Expression statement
+  ND_STMT_EXPR, // Statement expression
+  ND_VAR,       // Variable
+  ND_VLA_PTR,   // VLA designator
+  ND_NUM,       // Integer
+  ND_CAST,      // Type cast
+  ND_MEMZERO,   // Zero-clear a stack variable
+  ND_ASM,       // "asm"
+  ND_CAS,       // Atomic compare-and-swap
+  ND_EXCH,      // Atomic exchange
+} NodeKind;
